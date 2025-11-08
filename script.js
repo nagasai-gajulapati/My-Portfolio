de9d75eaca0614d2b5c17057a0ad2b67f7671bf5
nagasai9000 <nagasaigajulapati@gmail.com>
2025-11-08 16:44:08 +0530

    changed the contact section and added the gotoTop button

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
-
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+
+const backToTopBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.style.display = "flex";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+});
+
+backToTopBtn.onclick = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
